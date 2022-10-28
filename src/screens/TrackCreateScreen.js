3d1868b222c386e30416c274ab831f1c799064f1
Fabio Ernanni <ernannicf@gmail.com>
2022-10-28 14:50:23 -0300

    fake user location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import { requestForegroundPermissionsAsync } from 'expo-location';
 
+import '../_mockLocation';
 import { Map } from '../components';
 
 const TrackCreateScreen = () => {
+  const [error, setError] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) throw new Error('Location permission not granted');
+    } catch (e) {
+      setError(e);
+    }
+  }
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>Create a Track</Text>
       <Map />
+      {error && <Text>Please enable location services</Text>}
     </SafeAreaView>
   );
 }
